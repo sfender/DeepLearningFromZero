6e3ff677e3550b654cbaac016c80d522b1749b4b
sfender <k4219544@gmail.com>
2019-08-25 02:28:29 +0000

    relu関数追加

--- chapter3/activation_func.py
+++ chapter3/activation_func.py
@@ -9,6 +9,9 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
+
 #%% 
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
@@ -19,6 +22,13 @@ plt.show()
 #%% 
 x = np.arange(-5.0, 5.0, 0.1)
 y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+#%%
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
