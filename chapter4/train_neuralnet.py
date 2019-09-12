e237200df14788e77371a9ac668c64aaa9963e48
sfender <k4219544@gmail.com>
2019-09-12 13:52:39 +0000

    p 122まで

--- chapter4/train_neuralnet.py
+++ chapter4/train_neuralnet.py
@@ -1,4 +1,3 @@
-
 #%%
 import numpy as np
 from dataset.mnist import load_mnist
@@ -7,6 +6,9 @@ from chapter4.two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+iter_per_epoch = max(train_size/batch_size, 1)
 
 iters_num = 10
 train_size = x_train.shape[0]
@@ -27,6 +29,13 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    print('train acc, test acc | ' + str(train_acc) + ',' + str(test_acc))
 
 #%%
 print(train_loss_list)
