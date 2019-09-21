c4cba528c0c574d1a1d9f3704e86e2c15e437bd7
sfender <k4219544@gmail.com>
2019-09-21 03:51:24 +0000

    p157まで

--- chapter5/layer_naive.py
+++ chapter5/layer_naive.py
@@ -114,3 +114,46 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
 
         return dx
+
+#%%
+
+class Affine():
+
+    def __init__(self, W, b):
+        self.W  = W
+        self.b = b 
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+#%%
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.y = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = sotmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        
+        return dx
