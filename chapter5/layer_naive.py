0ddfe741cc6a7988e373a8d73b7590de8cfca512
sfender <k4219544@gmail.com>
2019-09-25 14:10:07 +0000

    p159 途中

--- chapter5/layer_naive.py
+++ chapter5/layer_naive.py
@@ -149,11 +149,43 @@ class SoftmaxWithLoss:
 
     def forward(self, x, t):
         self.t = t
-        self.y = sotmax(x)
+        self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
 
     def backward(self, dout = 1):
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
-        
+
         return dx
+
+#%%
+import sys,os
+sys.path.append(os.pardir)
+import numpy as np
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+
+class TwolayerNet:
+    
+    def __init__(self, input_size, hidden_size, output_size,
+                    weight_init_std=0.1):
+
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(input_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = \
+            Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = \
+            Affine(self.params['W2'], self.params['b2'])
+
+        self.lastlayer = SoftmaxWithLoss()
+    
+        
