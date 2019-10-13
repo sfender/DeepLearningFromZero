160c015e730894bd9e55929e1e89f56782fbd2a7
sfender <k4219544@gmail.com>
2019-10-13 06:41:06 +0000

    p160まで

--- chapter5/layer_naive.py
+++ chapter5/layer_naive.py
@@ -187,5 +187,51 @@ class TwolayerNet:
             Affine(self.params['W2'], self.params['b2'])
 
         self.lastlayer = SoftmaxWithLoss()
+
+def predict(self, x):
+    for layers in self.layers.values():
+        x = layer.forward(x)
     
-        
+    return x
+
+def loss(self, x, t):
+    y = self.predict(x)
+    return self.lastlayer.forward(y, t)
+
+def accuracy(self, x, y):
+    y = self.predict(x)
+    y = np.argmax(y, axis=1)
+    if t.ndim != 1 : t = np.argmax(t, axis=1)
+
+    accuracy = np.sum(y == t) / float(x.shape[0])
+    return accuracy
+
+def numerical_gradient(self, x, t):
+    loss_W = lambda W: self.loss(x, t)
+
+    grads = {}
+    grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+    grads['b1'] = numerical_gradient(loss_W, self.params['b1'])        
+    grads['W2'] = numerical_gradient(loss_W, self.params['W2'])       
+    grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+    return grads
+
+def gradient(self, x, t):
+    self.loss(x, t)
+
+    dout = 1
+    dout = self.lastlayer.backward(dout)
+
+    layers = list(self.layers.values())
+    layers.reverse()
+    for layer in layers:
+        dout = layer.backward(dout)
+
+    grads = {}
+    grads['W1'] = self.layers['Affine1'].dW
+    grads['b1'] = self.layers['Affine1'].db
+    grads['W2'] = self.layers['Affine2'].dW
+    grads['b2'] = self.layers['Affine2'].db
+
+    return grads
